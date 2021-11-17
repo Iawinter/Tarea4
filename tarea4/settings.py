7ac4cb1f84cc567598582e94cba40e73725d099a
iawinter <iawinter@uc.cl>
2021-11-17 16:15:31 -0300

    s

--- tarea4/settings.py
+++ tarea4/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-57(xy9v3z^g)@c=63k439r#mx!u4=4i!p0viuv5i1!ac%^mkv3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tarea4taller.herokuapp.com']
 
