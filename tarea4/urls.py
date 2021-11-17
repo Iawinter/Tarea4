d7a6ca3ebc1fd0f0c088797a4a248794f5c2d497
iawinter <iawinter@uc.cl>
2021-11-17 16:31:42 -0300

    s

--- tarea4/urls.py
+++ tarea4/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('pub/', include('pub.urls')),
+    path('pub', include('pub.urls')),
     path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
 ]
