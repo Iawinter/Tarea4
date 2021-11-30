fe573e3c264aaa9bfe667ca7f12467739ac09330
iawinter <iawinter@uc.cl>
2021-11-30 12:12:54 -0300

    w

--- pub/urls.py
+++ pub/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "pub"
 urlpatterns = [
-    path('', views.MensajesList, name='MensajesList'),
+    path('', views.MensajesList.as_view(), name='MensajesList'),
     path('elim/', views.eliminar_sus, name='eliminar_sus'),
     path('sus/', views.suscripcion, name='suscripcion'),
 ]
